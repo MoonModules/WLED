6d1f36d3e28ea5ba6e82b7de9b8a2154262a5083
Frank <91616163+softhack007@users.noreply.github.com>
2025-11-22 00:41:03 +0100

    third fix for drawCharacter 🫣
    time to get some sleep

--- wled00/FX_2Dfcn.cpp
+++ wled00/FX_2Dfcn.cpp
@@ -949,17 +949,18 @@ void Segment::drawCharacter(unsigned char chr, int16_t x, int16_t y, uint8_t w, 
     if (col2 != BLACK) col = ColorFromPalette(grad, (i+1)*255/h, 255, NOBLEND);
     uint32_t fgCol = uint32_t(col) & 0x00FFFFFF; // WLEDMM cache color value
 
-    for (int j = 0; j<w; j++) { // paint character - single row of pixels (width)
+    int numBits = (xoffset+1 < font.width_bytes) ? 8: (w % 8); // 8 for full bytes, width % 8 for last partial byte
+    for (int j = 0; j<numBits; j++) { // paint character - single row of pixels (width)
       int x0 = x + (w-1) - j + pixels_offset;
       if (unsigned(x0) < cols) { // WLEDMM same as "x0 > 0 && x0 < cols"
-        if ((bits>>(j+(8-w))) & 0x01) { // bit set & drawing on-screen
+        if ((bits>>(j+(8-numBits))) & 0x01) { // bit set & drawing on-screen
         setPixelColorXY(x0, y0, fgCol);
         } else {
           if (drawShadow) {
 			      // WLEDMM
-            if ((j < (w-1)) && (bits>>(j+(8-w) +1)) & 0x01) setPixelColorXY(x0, y0, bgCol); // blank when pixel to the right is set
-            else if ((j > 0) && (bits>>(j+(8-w) -1)) & 0x01) setPixelColorXY(x0, y0, bgCol);// blank when pixel to the left is set
-            else if ((bits_up>>(j+(8-w))) & 0x01) setPixelColorXY(x0, y0, bgCol);           // blank when pixel above is set
+            if ((j < (numBits-1)) && (bits>>(j+(8-numBits) +1)) & 0x01) setPixelColorXY(x0, y0, bgCol); // blank when pixel to the right is set
+            else if ((j > 0) && (bits>>(j+(8-numBits) -1)) & 0x01) setPixelColorXY(x0, y0, bgCol);// blank when pixel to the left is set
+            else if ((bits_up>>(j+(8-numBits))) & 0x01) setPixelColorXY(x0, y0, bgCol);           // blank when pixel above is set
           }
         }
       }
