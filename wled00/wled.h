1971b6d275cf71f03b9e9ce3e11b8a20691a0c11
Frank <91616163+softhack007@users.noreply.github.com>
2025-11-25 13:45:02 +0100

    disable ARDUINOJSON_DECODE_UNICODE when WLEDMM_SAVE_FLASH
    not sure if ARDUINOJSON_DECODE_UNICODE 1 is really needed - adds 1.2KB flash.

--- wled00/wled.h
+++ wled00/wled.h
@@ -176,8 +176,11 @@
 #ifdef WLED_ENABLE_MQTT
 #include "src/dependencies/async-mqtt-client/AsyncMqttClient.h"
 #endif
-
+#if defined(WLEDMM_SAVE_FLASH)
+#define ARDUINOJSON_DECODE_UNICODE 0   // WLEDMM not sure if this is really needed. disable saves 1.2KB flash
+#else
 #define ARDUINOJSON_DECODE_UNICODE 1   // WLEDMM enable unicode support -> prevents crashes when user enters unicode strings in webUI
+#endif
 #include "src/dependencies/json/AsyncJson-v6.h"
 #include "src/dependencies/json/ArduinoJson-v6.h"
 
