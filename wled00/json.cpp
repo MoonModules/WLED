75cbe6ced4b4c24f4b6a8ddcdf25f9c8521ca858
Frank <91616163+softhack007@users.noreply.github.com>
2026-02-16 20:05:34 +0100

    UI "free heap" was sometimes behind -> back to ESP.getFreeHeap()
    for UI consistency

--- wled00/json.cpp
+++ wled00/json.cpp
@@ -1128,8 +1128,7 @@ void serializeInfo(JsonObject root)
   #endif
   root[F("getflash")] = ESP.getFlashChipSize(); //WLEDMM and Athom, works for both ESP32 and ESP8266
 
-  //root[F("freeheap")] = ESP.getFreeHeap();
-  root[F("freeheap")] = getFreeHeapSize();
+  root[F("freeheap")] = ESP.getFreeHeap();
   //WLEDMM: conditional on esp32
   #if defined(ARDUINO_ARCH_ESP32)
     root[F("freestack")] = uxTaskGetStackHighWaterMark(NULL); //WLEDMM
